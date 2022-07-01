35d3e530f5c723e772940596006ba89ef860590b
Peyton Kellogg <pekellogg@hotmail.com>
2022-06-30 17:10:07 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const headerFive = document.createElement("h3");
+document.body.appendChild(headerFive);
+headerFive.innerHTML = "Does this work?"
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "PK is the champion!";
